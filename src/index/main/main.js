61d3a757f6480d1095cbc9653cbe6db515fe5709
shchik <nikshc2005@mail.ru>
2024-11-03 14:20:50 +0300

    ended sidebar main page and started footer for index

--- src/index/main/main.js
+++ src/index/main/main.js
@@ -1,6 +1,8 @@
 import favouritesImage from "./images/icons/favourites-icon.png";
 import likeImage from "./images/icons/like-icon.png";
 import infoImage from "./images/icons/info-icon.png";
+import searchImage from "./images/icons/search-icon.png";
+import activeImage from "./images/icons/active-icon.png";
 import { slots } from "../../data/slot";
 import "./main.css";
 
@@ -29,7 +31,56 @@ function Main() {
 
   return (
     <div className="main-flexbox">
-      <div className="search-sidebar"></div>
+      <div className="sidebar">
+        <div className="search-bar">
+          <img src={searchImage}></img>
+          <input placeholder="Провайдеры поиска"></input>
+        </div>
+        <ul className="genre-list">
+          <li className="one-genre">
+            <span>Драма</span>
+            <input type="radio" id="drama"></input>
+            <label for="drama" class="custom-label">
+              <img src={activeImage}></img>
+            </label>
+          </li>
+          <li className="one-genre">
+            <span>Комедия</span>
+            <input type="radio" id="comedy"></input>
+            <label for="comedy" class="custom-label">
+              <img src={activeImage}></img>
+            </label>
+          </li>
+          <li className="one-genre">
+            <span>Боевик</span>
+            <input type="radio" id="boevic"></input>
+            <label for="boevic" class="custom-label">
+              <img src={activeImage}></img>
+            </label>
+          </li>
+          <li className="one-genre">
+            <span>Триллер</span>
+            <input type="radio" id="triller"></input>
+            <label for="triller" class="custom-label">
+              <img src={activeImage}></img>
+            </label>
+          </li>
+          <li className="one-genre">
+            <span>Хоррор</span>
+            <input type="radio" id="horror"></input>
+            <label for="horror" class="custom-label">
+              <img src={activeImage}></img>
+            </label>
+          </li>
+          <li className="one-genre">
+            <span>Фантастика</span>
+            <input type="radio" id="fantasy"></input>
+            <label for="fantasy" class="custom-label">
+              <img src={activeImage}></img>
+            </label>
+          </li>
+        </ul>
+      </div>
 
       <div className="main-grid-layout js-main-grid-layout">
         {renderMainGrid()}
