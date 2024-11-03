0b6a2496c76b3b5c8a046ab21b2529797caad103
shchik <nikshc2005@mail.ru>
2024-11-03 14:59:08 +0300

    created footer on main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./index/header/header.js";
 import Main from "./index/main/main.js";
+import Footer from "./index/footer/footer.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
